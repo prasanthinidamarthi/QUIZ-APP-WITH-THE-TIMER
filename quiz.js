f4e158bd334c92949700b6d6449f04142035ac43
prasanthinidamarthi <79895055+prasanthinidamarthi@users.noreply.github.com>
2021-04-10 17:08:18 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,116 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+ {
+ numb: 1,
+ question: "What does HTML stand for?",
+ answer: "Hyper Text Markup Language",
+ options: [
+   "Hyper Text Preprocessor",
+   "Hyper Text Markup Language",
+   "Hyper Text Multiple Language",
+   "Hyper Tool Multi Language"
+ ]
+},
+ {
+ numb: 2,
+ question: "What does CSS stand for?",
+ answer: "Cascading Style Sheet",
+ options: [
+   "Common Style Sheet",
+   "Colorful Style Sheet",
+   "Computer Style Sheet",
+   "Cascading Style Sheet"
+ ]
+},
+ {
+ numb: 3,
+ question: "What does PHP stand for?",
+ answer: "Hypertext Preprocessor",
+ options: [
+   "Hypertext Preprocessor",
+   "Hypertext Programming",
+   "Hypertext Preprogramming",
+   "Hometext Preprocessor"
+ ]
+},
+ {
+ numb: 4,
+ question: "What does SQL stand for?",
+ answer: "Structured Query Language",
+ options: [
+   "Stylish Question Language",
+   "Stylesheet Query Language",
+   "Statement Question Language",
+   "Structured Query Language"
+ ]
+},
+ {
+ numb: 5,
+ question: "What does XML stand for?",
+ answer: "eXtensible Markup Language",
+ options: [
+   "eXtensible Markup Language",
+   "eXecutable Multiple Language",
+   "eXTra Multi-Program Language",
+   "eXamine Multiple Language"
+ ]
+},
+// you can uncomment the below codes and make duplicate as more as you want to add question
+// but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  {
+   numb: 6,
+   question: "Choose the correct html element tag with largest heading",
+   answer: "h1",
+   options: [
+     "h6",
+     "head",
+     "header",
+     "h1",
+   ]
+},
+{
+ numb: 7,
+ question: "An array with a numeric index is known as ___________ in PHP",
+ answer:"Numeric array",
+ options: [
+   "Numeric array",
+   "Multidimensional array",
+   "None",
+   "Associative array",
+ ]
+},
+{
+ numb: 8,
+ question: "Which of the following is not a 3 tier architecture?",
+ answer: "Communication layer",
+ options: [
+   "Communication layer",
+   "Business Logic layer",
+   "Presentation layer",
+   "Database layer",
+ ]
+},
+{
+ numb: 9,
+ question: "Which of the following is not a type of Firewalls?",
+ answer:"None",
+ options: [
+   "Application-level Gateways",
+   "Circuit-level Gateways",
+   "None",
+   "Packet Filters",
+ ]
+},
+{
+ numb: 10,
+ question: "WWW stands for ?",
+ answer:"World Wide Web",
+ options: [
+   "World Woman Wibe",
+   "World Web Wide",
+   "World Wide Web",
+   "Web Wide World",
+ ]
+}
+];
